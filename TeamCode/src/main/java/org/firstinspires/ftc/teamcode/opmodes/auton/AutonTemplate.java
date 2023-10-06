6b6d1d22c091b521db97ce914bbac6ebcdfa184a
javibavi <xavierwood3737@gmail.com>
2023-10-05 22:38:00 -0500

    updated auton template

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/AutonTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/AutonTemplate.java
@@ -3,9 +3,13 @@ package org.firstinspires.ftc.teamcode.opmodes.auton;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import java.util.List;
 
 @Autonomous
 public class AutonTemplate extends LinearOpMode {
@@ -16,10 +20,36 @@ public class AutonTemplate extends LinearOpMode {
 
     private double horizontalDistance;
 
+    private final double horizontalNegative = -1.25;
+
+    private final double horizontalPositive = 1.25;
+
+    private int spikeAndTagNum;
+
     @Override
     public void runOpMode(){
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        tfodProcessor = new TfodProcessor.Builder()
+                .build();
+
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), tfodProcessor);
+
+        while (!opModeIsActive()){
+
+            List<Recognition> recognitions = tfodProcessor.getRecognitions();
+
+            for (Recognition i: recognitions){
+
+
+            }
+
+        }
+
+        waitForStart();
+
+
 
     }
 
