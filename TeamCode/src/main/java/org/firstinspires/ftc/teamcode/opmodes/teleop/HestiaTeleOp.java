ef54fbda6d2a21575da46450bd5dc93896a1339f
javibavi <xavierwood3737@gmail.com>
2023-10-19 20:32:39 -0500

    Updated teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/HestiaTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/HestiaTeleOp.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
@@ -19,18 +20,30 @@ public class HestiaTeleOp extends OpMode {
     private DcMotor backRight;
     private DcMotor backLeft;
 
+    private DcMotor towerLeft;
+
+    private DcMotor slider;
+
+    private Servo launcher;
+
+    private Servo claw;
+
     // This is our timer used to track time in seconds
     private ElapsedTime timer = new ElapsedTime();
 
 
     // This is our led driver
-    private RevBlinkinLedDriver lights;
+//    private RevBlinkinLedDriver lights;
 
 
     // These variables will be used to track the movements of the joysticks
     private double forward;
     private double strafe;
     private double turn;
+
+    private double tower;
+
+    private double slide;
 
     // This variable will help me determine whether or not to reset the time
     private boolean isFirstLoop = true;
@@ -50,9 +63,18 @@ public class HestiaTeleOp extends OpMode {
         backLeft = hardwareMap.dcMotor.get("backLeft");
         backRight = hardwareMap.dcMotor.get("backRight");
 
+        towerLeft = hardwareMap.dcMotor.get("towerLeft");
+        slider = hardwareMap.dcMotor.get("slider");
+
+        launcher = hardwareMap.servo.get("launcher");
+        claw = hardwareMap.servo.get("claw");
+
+        towerLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
         // Hardware mapping of the driver and setting the default pattern
-        lights = hardwareMap.get(RevBlinkinLedDriver.class,"lights");
-        lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_HEARTBEAT_SLOW);
+//        lights = hardwareMap.get(RevBlinkinLedDriver.class,"lights");
+//        lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_HEARTBEAT_SLOW);
 
         // Sets the directions of the motors to be correct
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -65,6 +87,7 @@ public class HestiaTeleOp extends OpMode {
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
+        timer.reset();
 
     }
 
@@ -76,31 +99,60 @@ public class HestiaTeleOp extends OpMode {
         forward = gamepad1.left_stick_y;
         strafe = gamepad1.left_stick_x;
         turn = gamepad1.right_stick_x;
+        tower = gamepad2.left_stick_y;
+        slide = gamepad2.right_stick_y;
+
+
 
         // If this is the first loop where there is activity
-        if (isFirstLoop && (forward != 0 || strafe != 0 || turn != 0)){
-
-            // Reset the timer and add telemetry to show this
-            timer.reset();
-            telemetry.addLine("Timer has been reset");
-            telemetry.update();
-        }
-
-        // If the timer reaches 60 seconds and is less than 90 seconds, switch to the next pattern
-        if (timer.seconds()>=60 && timer.seconds()<90){
-            lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP2_HEARTBEAT_FAST);
-        }
-
-        // If the timer reaches 90 seconds, signal that it is now end game
-        if (timer.seconds()>=90){
-            lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_2_SPARKLE_1_ON_2);
-        }
+//        if (isFirstLoop && (forward != 0 || strafe != 0 || turn != 0)){
+//
+//            // Reset the timer and add telemetry to show this
+//            timer.reset();
+//            telemetry.addLine("Timer has been reset");
+//            telemetry.update();
+//        }
+//
+//        // If the timer reaches 60 seconds and is less than 90 seconds, switch to the next pattern
+//        if (timer.seconds()>=60 && timer.seconds()<90){
+//            lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP2_HEARTBEAT_FAST);
+//        }
+//
+//        // If the timer reaches 90 seconds, signal that it is now end game
+//        if (timer.seconds()>=90){
+//            lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_2_SPARKLE_1_ON_2);
+//        }
 
         // Sets the power of our motors to our variables we created earlier
         frontRight.setPower(forward + strafe + turn);
         frontLeft.setPower(forward - strafe - turn);
         backRight.setPower(forward - strafe + turn);
         backLeft.setPower(forward + strafe - turn);
+
+
+        towerLeft.setPower(tower);
+
+        slider.setPower(slide);
+
+        // Code to launch the servo
+        if ((gamepad2.dpad_up)){
+            launcher.setPosition(.25);
+        }
+
+        // Code to reset the servo
+        if (gamepad2.dpad_down){
+            launcher.setPosition(1);
+        }
+
+        // Open claw
+        if (gamepad2.right_bumper){
+            claw.setPosition(.72);
+        }
+
+        // Close claw
+        if (gamepad2.left_bumper){
+            claw.setPosition(.42);
+        }
 
 
 
